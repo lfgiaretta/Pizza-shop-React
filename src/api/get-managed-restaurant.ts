a2cf1d3da3bae12df0b541b9b0c89cb9764b6d17
Luis Giaretta <lfgiaretta@gmail.com>
2024-05-02 16:48:40 -0300

    lfgiaretta: pizza-shop project without tests

---
+++ src/api/get-managed-restaurant.ts
@@ -0,0 +1,18 @@
+import { api } from '@/lib/axios'
+
+export interface GetManagedRestaurantResponse {
+  id: string
+  name: string
+  createdAt: Date | null
+  updateAt: Date | null
+  description: string | null
+  managerId: string | null
+}
+
+export async function getManagedRestaurant() {
+  const response = await api.get<GetManagedRestaurantResponse>(
+    '/managed-restaurant ',
+  )
+
+  return response.data
+}
